7c65a780c5067d113badebef00f14c8b7c1ce677
Sudheerchikile <129176642+Sudheerchikile@users.noreply.github.com>
2024-05-17 13:02:10 +0300

    added sample search keyword element

--- src/components/ImageSearchComponent/index.js
+++ src/components/ImageSearchComponent/index.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import './index.css';
 import { FidgetSpinner } from 'react-loader-spinner';
 
+
 const ImageSearch = () => {
   const [query, setQuery] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-
+  
   useEffect(() => {
     if (searchQuery) {
       fetchImages(searchQuery);
@@ -24,6 +25,7 @@ const ImageSearch = () => {
       const apikey = `tF914_18ls1qqSBbA1GpPSt4Haq2bGrqwV2NItnlAdA`;
       const response = await axios.get(`https://api.unsplash.com/search/photos?client_id=${apikey}&query=${searchQuery}`);
       setImages(response.data.results);
+      console.log(response.data);
       setQuery('');
     } catch (err) {
       setError('Failed to fetch images. Please try again.');
@@ -34,6 +36,7 @@ const ImageSearch = () => {
 
   const handleSearch = (e) => {
     e.preventDefault();
+    
     if (query.trim()) {
       setSearchQuery(query);
     } else {
@@ -50,8 +53,19 @@ const ImageSearch = () => {
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Search for images..."
         />
-        <button type="submit">Search</button>
+        <button type="submit" className='search-button'>Search</button>
+
+
+        <div className='buttons-con'>
+       
+       <button onClick={(e) => setQuery('Mountains')}>Mountains</button>
+       <button onClick={(e) => setQuery('Trees')}>Trees</button>
+       <button onClick={(e) => setQuery('Beaches')}>Beaches</button>
+       <button onClick={(e) => setQuery('Animals')}>Animals</button>
+      
+     </div>
       </form>
+     
 
       {loading && (
         <div className='loading-con'>
@@ -67,9 +81,12 @@ const ImageSearch = () => {
       )}
 
       {error && <p className="error-message">{error}</p>}
+      <h1 className='search-heading'>{searchQuery}</h1>
 
       {images.length > 0 && (
+        
         <div className="image-grid">
+          
           {images.map((image) => (
             <div key={image.id} className="image-container">
               <img src={image.urls.small} alt={image.alt_description} />
@@ -81,6 +98,7 @@ const ImageSearch = () => {
           ))}
         </div>
       )}
+       
     </div>
   );
 };
